0d4de806aa652521692684bf65bfc736b3db1392
Eremin <haru.eaa@gmail.com>
2025-02-20 20:03:32 +0500

    feat: base/shared module call

---
+++ deployment/terraform/workloads/development/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-a6490666acaa9e18f19bdc1559e7c3acde30c9de"
+    key     = "terraform/ecs-sample/main.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+    dynamodb_table = "terraform-state-lock-a6490666acaa9e18f19bdc1559e7c3acde30c9de"
+  }
+}
---
+++ deployment/terraform/workloads/development/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ou_info" {
+  source  = "fivexl/shared-parameters/aws//modules/shared_parameter_data"
+  version = "0.0.5"
+
+  resource_share_name = "ou_info"
+}
+
+locals {
+  ou_info = nonsensitive(jsondecode(module.ou_info.value))
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+module "workloads" {
+  source = "../shared"
+
+  networking_account_id         = tostring(local.ou_info.accounts["tools-network"].id)
+  allow_ecr_get_for_account_ids = []
+  vpc_id                        = "vpc-0618018ee2bdea049"
+  dns_zone_name                 = "fivexl.dev"
+  ecr_image_uri                 = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/nginx:latest"
+
+  vpc = {
+    name = "apps"
+    cidr = "10.112.0.0/16"
+  }
+
+  db = {
+    engine_version                        = "16.3"
+    instance_class                        = "db.t4g.micro"
+    allocated_storage                     = 20
+    auto_minor_version_upgrade            = true
+    allow_major_version_upgrade           = false
+    apply_immediately                     = true
+    backup_retention_period               = 7
+    performance_insights_enabled          = true
+    performance_insights_retention_period = 7
+    enchanced_monitoring_interval         = 60
+    multi_az                              = false
+    max_allocated_storage                 = 500
+  }
+
+  tags = {
+    environment_name = "development"
+    environment_type = "workloads"
+    data_pci         = "0"
+    data_phi         = "0"
+    data_pii         = "0"
+    prefix           = "fivexl"
+    data_owner       = "fivexl.com"
+  }
+  primary_region   = "us-east-1"
+  secondary_region = "eu-west-1"
+}
+
