a22df02a4dc3f15cf273b34821d4d597aab15f8c
Alexei Eremin <haru.eaa@gmail.com>
2025-03-13 12:00:27 +0500

    feat: aws account id from aws sts get-caller-identity

--- deployment/terraform/workloads/shared/ghcr-to-ecr.sh
+++ deployment/terraform/workloads/shared/ghcr-to-ecr.sh
@@ -2,7 +2,7 @@
 
 # Define variables
 AWS_REGION="us-east-1"
-AWS_ACCOUNT_ID=""
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 ECR_REPOSITORY_PREFIX="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
