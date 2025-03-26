96b7bbc1af436388ca8c6276ed322e1839f99924
Alexei Eremin <haru.eaa@gmail.com>
2025-03-26 11:34:27 +0500

    feat: gateway * route53 record

--- deployment/terraform/workloads/shared/route53.tf
+++ deployment/terraform/workloads/shared/route53.tf
@@ -14,3 +14,18 @@ resource "aws_route53_record" "services" {
     zone_id                = module.ingress_alb.zone_id
   }
 }
+
+resource "aws_route53_record" "gateway" {
+  provider = aws.dns
+
+  zone_id  = data.aws_route53_zone.primary_public.zone_id
+  name     = "*.${data.aws_route53_zone.primary_public.name}"
+
+  type     = "A"
+
+  alias {
+    evaluate_target_health = true
+    name                   = module.ingress_alb.dns_name
+    zone_id                = module.ingress_alb.zone_id
+  }
+}
